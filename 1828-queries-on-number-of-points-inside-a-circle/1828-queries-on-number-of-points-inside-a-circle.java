6fa2962973272cca9b670b7a6d75eced61101fde
Prashant Kumar <kumarprashant8595@gmail.com>
2026-09-03 14:59:00 +0530

    Time: 15 ms (93.85%), Space: 47.3 MB (25.13%) - LeetHub

---
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] countPoints(int[][] points, int[][] queries) {
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int cx = queries[i][0];
+            int cy = queries[i][1];
+            int r = queries[i][2];
+
+            for (int[] point : points) {
+                int dx = point[0] - cx;
+                int dy = point[1] - cy;
+
+                if (dx * dx + dy * dy <= r * r) {
+                    ans[i]++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
