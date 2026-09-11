4e036d4c37ef6050dcffe626a212aa7d93d4731f
Prashant Kumar <kumarprashant8595@gmail.com>
2026-09-11 13:24:07 +0530

    Time: 2 ms (93.87%), Space: 44.9 MB (81.2%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        boolean[] used = new boolean[1000];
+        int count = 0;
+
+        for (int i = 0; i < digits.length; i++) {
+
+            // Hundreds digit cannot be 0
+            if (digits[i] == 0) {
+                continue;
+            }
+
+            for (int j = 0; j < digits.length; j++) {
+
+                // Cannot use the same index twice
+                if (j == i) {
+                    continue;
+                }
+
+                for (int k = 0; k < digits.length; k++) {
+
+                    // Cannot use the same index twice
+                    if (k == i || k == j) {
+                        continue;
+                    }
+
+                    // Last digit must be even
+                    if (digits[k] % 2 != 0) {
+                        continue;
+                    }
+
+                    int num = digits[i] * 100
+                            + digits[j] * 10
+                            + digits[k];
+
+                    // Count each number only once
+                    if (!used[num]) {
+                        used[num] = true;
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
